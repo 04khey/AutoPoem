9bb886dc0f202ec085520b77202412d2275c3e0b
Dmitry <PenguinProductions00@gmail.com>
2025-01-18 10:41:04 +0000

    Small cleanup

--- Autopoem.cpp
+++ Autopoem.cpp
@@ -5,15 +5,12 @@
 
 //c++ -O2 -o autopoem Autopoem.cpp `Magick++-config --cppflags --cxxflags --ldflags --libs`
 
- #include <ImageMagick-7/Magick++.h> //ImageMagick-7/
- #include <iostream>
+#include <ImageMagick-7/Magick++.h> 
+#include <iostream>
 #include <fstream>
-#include <queue>
+//#include <queue>
 #include <vector>
-//#include <qt6/QtCore/qfile.h>
-//#include <qt6/qfile>
-//#include <qt6/QtCore/QFile>
-//#include <qt6/QtCore>
+
 
  using namespace Magick;
  using namespace std;
@@ -23,7 +20,7 @@ vector<string> poem;
 
 int readPoem(){
      //ifstream f(argv[1] + ".txt");
-    ifstream f("seapoemspooky.txt");
+    ifstream f("seapoemspooky.txt"); //debug
 
     if (!f.is_open()) {
         cerr << "read error\n";
@@ -32,16 +29,11 @@ int readPoem(){
     string s;
 
     for (int i=0;getline(f, s) ;i++){
-        //s = getline(f, s)
+
         poem.push_back(s);
-        //poem[i] << s << endl;
         //poem[i] = s;
     }
-    /*
-    getline(f,poem[0]);
-    getline(f,poem[1]);
-    getline(f,poem[2]);
-*/
+
     f.close();
 
     return 0;
@@ -50,7 +42,6 @@ int readPoem(){
 
 int main( ssize_t argc, char ** argv)
 {
-    //vector<string> poem;
     
     if(readPoem()){
         return -1;
@@ -62,18 +53,24 @@ int main( ssize_t argc, char ** argv)
    
 
     InitializeMagick(*argv);
-    //std::cout << poem.size() << std::endl;
 
-    Image my_image( Geometry(320,220), Color("white"));
+
+    Image my_image( Geometry(1080,1080), Color("white"));
     // set the text rendering font (the color is determined by the "current" image setting)
     my_image.font("EVA-Matisse_Standard-EB");
+    my_image.fontPointsize(100);
     // draw text with different gravity position
     my_image.annotate("NorthWest gravity annotation", NorthWestGravity);
     my_image.annotate("SouthEast gravity annotation", SouthEastGravity);
-   
-    my_image.annotate(poem[0], CenterGravity);
+    
+    //https://imagemagick.org/Magick++/Geometry.html
 
-    my_image.write("file_name_explicit_extension.gif");
+    Geometry test("1080x200+0+100");
+
+    //my_image.annotate(poem[0], CenterGravity);
+    my_image.annotate(poem[0], test, CenterGravity);
+
+    my_image.write("out.png");
 
     return 0;
 }

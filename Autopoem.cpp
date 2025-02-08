78dd2dc6b77b5aac779af2106798b229f8a1a674
Dmitry <PenguinProductions00@gmail.com>
2025-02-08 21:29:24 +0000

    some cleanup. Committing to verse idea.

--- Autopoem.cpp
+++ Autopoem.cpp
@@ -14,20 +14,35 @@
  using namespace Magick;
  using namespace std; //this is supposedly bad practice. But this is a small project, so should be fine?
 
+struct lineMaxes{
+    int longestLine = 0;
+    int tallestLine = 0;
 
-vector<string> poem; //should this be vector<string*>? Probs not. Surely string* is the default. 
-int sideMarginPx = 100;
+    int maxWidth = 0;
+    int maxHeight = 0;
+};
+
+//PARAMETERS
+int sideMarginPx = 0;
 int topMarginPx = 200;
-int lineSpacing = 20;
-vector<int> runs;
+int lineSpacing = 60;
 
+const string fontFilename = "EVA-Matisse_Standard-EB";
+
+bool doPerImageFontTargets =true;
+bool centreVerticalAlign = true;
+
+GravityType desiredGravity = NorthGravity; //for verse alignment
+//GravityType desiredGravity = NorthWestGravity; //for verse alignment
+
+//GLOBALS
 vector<vector<string>> verses;
+vector<lineMaxes> verseMaxes;
+vector<int> numVersesInImages;
 
 
 
 int readPoem(){
-    
-    int runCounter=0;
 
      //ifstream f(argv[1] + ".txt");
     ifstream f("seapoemspooky.txt"); //debug
@@ -37,30 +52,36 @@ int readPoem(){
         return -1;
     }
     string s;
+    bool newVerseFlag = 0;
+    vector<string>* currentVerse;
+    currentVerse = new vector<string>; 
 
     for (int i=0;getline(f, s) ;i++){
-
-        if(s == ""){ //empty line
-            runs.push_back(runCounter);
-            runCounter = 0;
-
-        } else {
-            runCounter++;
+        
+        //if(((*currentVerse).size() > 0)&&(newVerseFlag)){
+        if((newVerseFlag) && s!= ""){
+                verses.push_back(*currentVerse);
+                currentVerse = new vector<string>; 
+                newVerseFlag = 0;
         }
 
-        poem.push_back(s);
-        //poem[i] = s;
+        if(s == ""){ 
+            newVerseFlag = 1;
+            
+        } else {
+           (*currentVerse).push_back(s); 
+        }
+
     }
-    runs.push_back(runCounter + 1); //+1 to account for final line
 
-
+    verses.push_back(*currentVerse);
 
     f.close();
 
     return 0;
 }
 
-void printVector(vector<string> in){ //todo: use autos for indices, replace some of these vectors with std::arrays
+void printVector(vector<string> in){ 
     for(int i=0;i<in.size();i++){
         std::cout << i << ": " << in[i];
     }
@@ -84,7 +105,6 @@ int getLongestLineLength(int ptSize, vector<string> lines, int numLines, Image i
     TypeMetric currLineMetrics;
     int length = 0;
 
-    std::cout << "HERE: .size()=" << lines.size() << ", numLines= " << numLines << "\n"; 
 
     for(int i=0;i< numLines;i++){
         image.fontTypeMetrics(lines.at(i), &currLineMetrics);
@@ -98,7 +118,7 @@ int getLongestLineLength(int ptSize, vector<string> lines, int numLines, Image i
 }
 
 int getPtSizeTarget(string longestLine, Image image){
-    int targetWidth = 1080 - 2 * sideMarginPx;
+    int targetWidth = 1080 - (2 * sideMarginPx);
     //size_points = (size_pixels * 72)/resolution
 
 
@@ -145,6 +165,26 @@ int getMaxPageLines(string tallestLine, Image image){
     return maxLines;
 }
 
+int getMinElementInArray(int* inpArray, int size){
+    int min = inpArray[0];
+    for(int i=0; i <size; i++){
+        if(inpArray[i] < min){
+            min = inpArray[i];
+        }
+    }
+    return min;
+}
+
+int getMaxElementInArray(int* inpArray, int size){
+    int max = inpArray[0];
+    for(int i=0; i <size; i++){
+        if(inpArray[i] > max){
+            max = inpArray[i];
+        }
+    }
+    return max;
+}
+
 vector<int> squeezeRuns(int maxLines, vector<int> runsIn){
     int runningCount =0;
     vector<int> squeezed;
@@ -174,28 +214,75 @@ vector<int> squeezeRuns(int maxLines, vector<int> runsIn){
     return squeezed;
 }
 
-void createImage(vector<string> lines, int fontSize, int imageNum, int numLines, int maxHeight){
 
-    trimArray(lines);
+lineMaxes getLineMaxes(vector<string> verse, Image* my_image, TypeMetric* currLineMetrics){
+    int currWidth =0;
+    int currHeight = 0;
+
+    lineMaxes currLineMaxes;
+    for(int i=0;i<verse.size();i++){
+            my_image->fontTypeMetrics(verse[i], currLineMetrics);
+            int currWidth = currLineMetrics->textWidth();
+            if(currWidth > currLineMaxes.maxWidth ){
+                currLineMaxes.maxWidth = currWidth;
+                currLineMaxes.longestLine = i;
+            }
+            int currHeight = currLineMetrics->textHeight();
+            if(currHeight > currLineMaxes.maxHeight ){
+                currLineMaxes.maxHeight = currHeight;
+                currLineMaxes.tallestLine = i;
+            }
+            //std::cout<< i << ": " << verse[i] << " -- " << currWidth << "\n";
+        }
+    return currLineMaxes;
+}
+
+void createImage(vector<string> lines, int fontSize, int imageNum, int maxHeight){
 
     Image outImage( Geometry(1080,1080), Color("white"));
-    // set the text rendering font (the color is determined by the "current" image setting)
-    outImage.font("EVA-Matisse_Standard-EB");
+    //outImage.font("EVA-Matisse_Standard-EB");
+    outImage.font(fontFilename);
     outImage.fontPointsize(fontSize);
 
-    int maxLength = getLongestLineLength(fontSize, lines, numLines, outImage);
+    int numLines = lines.size();
 
-    int topOffset = (1080 - topMarginPx - (numLines * (maxHeight + lineSpacing) * 2));
-    std::cout << numLines <<" lines"<<", first line: " << lines[0] << "last line: " << lines[numLines - 1] << "\noffset: " << topOffset << "px\n";
+    int centralisationOffset=0;
+    if(centreVerticalAlign){
+        int bottomOfText = topMarginPx + ((numLines+1-1) *(maxHeight + lineSpacing));
+        int bottomPossible = 1080 - maxHeight - topMarginPx;
+        centralisationOffset = (bottomPossible-bottomOfText)/2;
+    }
+
+
+    TypeMetric tm;
+
+
+    //int maxLength = getLongestLineLength(fontSize, lines, numLines, outImage);
+    int startVerseIndex = 0;
+    for(int i=0;i<imageNum;i++){
+        startVerseIndex += numVersesInImages[i];
+    }
+
+    int verseMaxLineLengths[numVersesInImages[imageNum]];
+    for(int i=startVerseIndex;i<numVersesInImages[imageNum]+startVerseIndex;i++){
+        lineMaxes tempLineMaxes = getLineMaxes(verses[i], &outImage, &tm);
+        verseMaxLineLengths[i-startVerseIndex] = tempLineMaxes.maxWidth;
+    }
+
+    int maxLength = getMaxElementInArray(verseMaxLineLengths, numVersesInImages[imageNum]);
+
+    //std::cout << "max line length for image " << imageNum << ": " << maxLength << "\n";
+
+
+    std::cout << numLines <<" lines"<<", first line: " << lines[0] << "last line: " << lines[numLines - 1] << "\n";
 
     for(int i=0;i<numLines;i++){
-        Geometry currTextGeom(1080, 1080, sideMarginPx + (1080 - (2 * sideMarginPx) - maxLength)/2, -(1080/2) + topMarginPx + topOffset+ (i *(maxHeight + lineSpacing))); //w, h, xoff, yoff
-        //Geometry currTextGeom(1080, 1080, sideMarginPx,  topMarginPx + (i *(maxHeight + lineSpacing))); //w, h, xoff, yoff
-        outImage.annotate(lines.at(i), currTextGeom, WestGravity);
+        int tempwidth = maxLength;
+        Geometry currTextGeom(tempwidth, 1080, (1080/2 - maxLength/2), topMarginPx + (i *(maxHeight + lineSpacing))+centralisationOffset ); //w, h, xoff, yoff
+        outImage.annotate(lines.at(i), currTextGeom, desiredGravity);
     }
-    //my_image.annotate(test, testGeom, WestGravity);
 
-    char imNum[5];
+    char imNum[5]; //will go out of bounds if > 99,999 images but I think there will be other problems then
     sprintf(imNum, "%d\0", imageNum);
     string str(imNum);
 
@@ -203,18 +290,67 @@ void createImage(vector<string> lines, int fontSize, int imageNum, int numLines,
     
 }
 
-void createImages(vector<int> squeezedLines, int fontSize, int maxHeight){
-    int numRequired = squeezedLines.size();
-    int count = 0;
-    for(int i=0;i<squeezedLines.size(); i++){
+void createImages(vector<int> numVersesInImages, int fontSize, int maxHeight){
+
+    int verseIndex =0;
+
+    //used to determine per-image font size
+    Image testImage( Geometry(1080,1080), Color("white"));
+    testImage.font(fontFilename);
+
+
+    for(int i=0;i<numVersesInImages.size(); i++){
+
         vector<string> lines;
 
-        for(int j = 0; j < squeezedLines[i];j++){
-            lines.push_back(poem[count]);
-            count++;
+          if(doPerImageFontTargets){ //TODO. 
+            int possibleFontSizeTargets[numVersesInImages[i]];
+            for(int j=0;j<numVersesInImages[i];j++){
+                possibleFontSizeTargets[j] = getPtSizeTarget(verses[j+verseIndex][verseMaxes[j+verseIndex].longestLine], testImage);
+            }
+            fontSize = getMinElementInArray(possibleFontSizeTargets, numVersesInImages[i]);
+          }
+
+        for(int j = 0; j < numVersesInImages[i];j++){
+            
+            for(string s : verses[verseIndex+j]){
+                lines.push_back(s);
+            }
+            if(j != numVersesInImages[i]-1){
+                lines.push_back("\n"); 
+            }
+              
         }
 
-        createImage(lines, fontSize, i, squeezedLines[i], maxHeight);
+
+        createImage(lines, fontSize, i, maxHeight);
+
+        verseIndex += numVersesInImages[i];
+    }
+
+
+}
+
+void genNumVersesInImages(vector<vector<string>> verses, int maxLines){
+    int currentVerseCount=0;
+    int currentLineCount=0;
+
+    for(vector<string> verse : verses){
+        //std::cout<< "verse starting \n"<<verse[0] << "\n.verseCount: " << currentVerseCount << ", lineCount: " << currentLineCount << "\n";
+        //TODO: if verse longer than maxlines, chunk verse and generate new verses vector
+        if((verse.size() + 1 + currentLineCount > maxLines) && currentVerseCount>0){
+            numVersesInImages.push_back(currentVerseCount);
+            currentVerseCount = 1;
+            currentLineCount = verse.size(); 
+            
+        } else {
+            currentLineCount = currentLineCount + 1 + verse.size();
+            currentVerseCount++;
+        }
+    }
+
+    if(currentVerseCount > 0){
+        numVersesInImages.push_back(currentVerseCount);
     }
 
 
@@ -227,8 +363,15 @@ int main( ssize_t argc, char ** argv)
         return -1;
     }
 
-    for(int i =0;i<runs.size();i++){
-        std::cout<< runs[i] << ",";
+    int lineCounter=0;
+    
+    for(int i =0;i<verses.size();i++){
+        std::cout<< "verses[" << i << "] size: " << verses[i].size() << "\n";
+        for(int j =0;j<verses[i].size();j++){
+            std::cout<< lineCounter << ": " << verses[i][j] << "\n";
+            lineCounter++;
+        }
+        
     } std::cout<< "\n";
 
     InitializeMagick(*argv);
@@ -236,64 +379,48 @@ int main( ssize_t argc, char ** argv)
 
     Image my_image( Geometry(1080,1080), Color("white"));
     // set the text rendering font (the color is determined by the "current" image setting)
-    my_image.font("EVA-Matisse_Standard-EB");
+    my_image.font(fontFilename);
     
     
     
     //https://imagemagick.org/Magick++/Geometry.html
 
-    int longestLine = 0;
-    int tallestLine = 0;
-
-    int maxWidth = 0;
-    int maxHeight = 0;
-    
-
     TypeMetric currLineMetrics;
 
-    for(int i=0;i<poem.size();i++){
-        
-        my_image.fontTypeMetrics(poem[i], &currLineMetrics);
-        int currWidth = currLineMetrics.textWidth();
-        if(currWidth > maxWidth ){
-            maxWidth = currWidth;
-            longestLine = i;
-        }
-        int currHeight = currLineMetrics.textHeight();
-        if(currHeight > maxHeight ){
-            maxHeight = currHeight;
-            tallestLine = i;
-        }
-        std::cout<< i << ": " << poem[i] << " -- " << currWidth << "\n";
+    for(vector<string> verse : verses){
+        verseMaxes.push_back(getLineMaxes(verse, &my_image, &currLineMetrics));
     }
 
-    std::cout<< "longest line: " << longestLine << "\n" << maxWidth << "\n";
+    for(int i=0;i<verses.size();i++){
+        std::cout<< "verse " << i <<" longest line:" << verseMaxes[i].longestLine << ", maxWidth:" << verseMaxes[i].maxWidth << "\n";
+    }
 
-    int ptSize = getPtSizeTarget(poem[longestLine], my_image);
+    //get longest verse for point size calculation.
+    int longestVerse = 0;
+    int longestVerseLine = verseMaxes[0].maxWidth;
+    for(int i=1;i<verseMaxes.size();i++){
+        if(verseMaxes[i].maxWidth > longestVerseLine){
+            longestVerseLine = verseMaxes[i].maxWidth;
+            longestVerse = i;
+        }
+    }
+    std::cout<< "longest verse: " << longestVerse << ", longest line: " << longestVerseLine << "\n";
+
+    int ptSize = getPtSizeTarget(verses[longestVerse][verseMaxes[longestVerse].longestLine], my_image);
     my_image.fontPointsize(ptSize);
 
-    int maxLines = getMaxPageLines(poem[tallestLine], my_image);
+    int maxLines = getMaxPageLines(verses[longestVerse][verseMaxes[longestVerse].longestLine], my_image);
 
-    vector<int> optimumRuns = squeezeRuns(maxLines, runs);
 
-    for(int i : optimumRuns){
+    genNumVersesInImages(verses, maxLines);
+
+    for(int i : numVersesInImages){
         std::cout<< i << ",";
     }
     
     std::cout<< "max page lines: " << maxLines << "\n";
 
-    createImages(optimumRuns, ptSize, maxHeight);
-
-    Geometry testGeom("1080x200+0+100");
-    //my_image.annotate(poem[longestLine], CenterGravity);
-
-    string test = "line 1\nline 2 blah blah blah\nline 3";
-
-    my_image.annotate(test, testGeom, WestGravity);
-
-    //my_image.annotate(poem[longestLine], test, CenterGravity);
-
-    my_image.write("out.png");
+    createImages(numVersesInImages, ptSize, verseMaxes[0].maxHeight);
     
 
     return 0;
--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: test 
 
 default:
-	c++ -O2 -o autopoem Autopoemv2.cpp `Magick++-config --cppflags --cxxflags --ldflags --libs`
+	c++ -O2 -o autopoem Autopoem.cpp `Magick++-config --cppflags --cxxflags --ldflags --libs`
 test:
 	c++ -O2 -o test Testing.cpp `Magick++-config --cppflags --cxxflags --ldflags --libs`
